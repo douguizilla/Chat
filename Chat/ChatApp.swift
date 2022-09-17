45fda2591735234cdfd0608075e53a24cb573486
Douglas Gomes de Paula <douglas.gomes@optimumsolucoes.com>
2022-09-17 13:37:48 -0300

    Initial Commit

---
+++ Chat/ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatApp.swift
+//  Chat
+//
+//  Created by Douglas Gomes on 17/09/22.
+//
+
+import SwiftUI
+
+@main
+struct ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
