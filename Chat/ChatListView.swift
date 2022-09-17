762ac4034b56bc9f89fcadb51f40152cf7638cd6
Douglas Gomes <douglasgommez@gmail.com>
2022-09-17 14:01:16 -0300

    Renamed files. ChatListView base was implemented.

---
+++ Chat/ChatListView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  Chat
+//
+//  Created by Douglas Gomes on 17/09/22.
+//
+
+import SwiftUI
+
+struct ChatListView: View {
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(0 ..< 10) { i in
+                    Text(i.description)
+                }
+            }
+            .listStyle(PlainListStyle())
+            .navigationTitle("Chats")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button{
+                        
+                    }label: {
+                        Image(systemName: "square.and.pencil")
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ChatListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChatListView()
+    }
+}
