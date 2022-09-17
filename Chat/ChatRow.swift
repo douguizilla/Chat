149c198712fcf4909a82e7c9db3c6498bebc8498
Douglas Gomes <douglasgommez@gmail.com>
2022-09-17 14:16:04 -0300

    Add avatar samples. ChatRow created but still being implemented

---
+++ Chat/ChatRow.swift
@@ -0,0 +1,22 @@
+//
+//  ChatRow.swift
+//  Chat
+//
+//  Created by Douglas Gomes on 17/09/22.
+//
+
+import SwiftUI
+
+struct ChatRow: View {
+    var body: some View {
+        HStack{
+            
+        }
+    }
+}
+
+struct ChatRow_Previews: PreviewProvider {
+    static var previews: some View {
+        ChatRow()
+    }
+}
